fb4a13703f03e79a4219eddcdf30c0950c2bc7c0
gvgeorge <gvgeorge@mail.ru>
2022-05-19 21:26:24 +0300

    removed old files

--- create_db.py
+++
@@ -1,5 +0,0 @@
-from database import engine
-from tables import Base
-
-
-Base.metadata.create_all(engine)
--- models/rubric_base.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class RubricBase(BaseModel):
-    name: str
-
-    class Config:
-        orm_mode = True
